fbd7aaa4dc300fd6f6109a30fe79c5e35d16dd76
Arian <arian.kulmer@web.de>
2021-03-12 20:12:07 +0100

    cupid: Configure screen density
    Set the screen density according to ro.sf.lcd_density from miui.
    
    Change-Id: Ic11b052d132f150da7070e5b8669e5a229f7528d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,3 +11,6 @@ include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 include vendor/xiaomi/cupid/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/cupid
+
+# Screen density
+TARGET_SCREEN_DENSITY := 440
