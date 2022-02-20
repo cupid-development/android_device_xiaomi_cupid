fee2b5f665484d9ca91ca79227a4c2fe4d17873e
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    cupid: Add support for udfps long press sensor
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- device.mk
+++ device.mk
@@ -20,6 +20,13 @@ PRODUCT_PACKAGES += \
     SystemUIResCupid \
     WifiResCupid
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
