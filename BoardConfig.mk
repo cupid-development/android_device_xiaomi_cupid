918fa12d9617454cdec6db0d2c792d98d3253325
Arian <arian.kulmer@web.de>
2022-04-03 18:42:06 +0200

    cupid: Initial device tree
    Change-Id: Id01117ad481165bb7a87cf3d2b43c7d9a5fa6d45

---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from xiaomi sm8450-common
+include device/xiaomi/sm8450-common/BoardConfigCommon.mk
+
+# Inherit from the proprietary version
+include vendor/xiaomi/cupid/BoardConfigVendor.mk
+
+DEVICE_PATH := device/xiaomi/cupid
