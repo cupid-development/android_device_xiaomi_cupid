f5ec0bd02d968ffe4c1f79755fffe72b63771030
Arian <arian.kulmer@web.de>
2022-04-03 18:42:06 +0200

    cupid: Initial device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_cupid.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_cupid-user \
+    lineage_cupid-userdebug \
+    lineage_cupid-eng
