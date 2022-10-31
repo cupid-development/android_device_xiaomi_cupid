cb2b49fa1f31ef887e6606963b45d09cee33a266
LuK1337 <priv.luk@gmail.com>
2022-10-31 21:59:52 +0100

    cupid: overlay: Configure aux camera for Aperture
    Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/xiaomi/cupid/cupid-vendor.mk)
 
 # Overlay
 PRODUCT_PACKAGES += \
-    CupidWifiOverlay \
+    ApertureResCupid \
     FrameworksResCupid \
     SettingsProviderResCupid \
     SystemUIResCupid \
