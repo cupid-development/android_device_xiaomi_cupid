f565f4fd3233b177d34b5f53f057b19c46c4f63e
Arian <arian.kulmer@web.de>
2023-06-04 14:50:45 +0200

    cupid: Import decommonized blobs
    Change-Id: I22a97a25cef551beb4f0d3897ee63acc6b474180

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,12 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/etc/camera/cupid_enhance_motiontuning.xml|vendor/etc/camera/cupid_motiontuning.xml)
+            sed -i 's/xml=version/xml version/g' "${2}"
+            ;;
+        vendor/etc/camera/pureShot_parameter.xml|vendor/etc/camera/pureView_parameter.xml)
+            sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
+            ;;
     esac
 }
 
