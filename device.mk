7f78eda1263de76da05ea98bacf3af1b09db01e0
Arian <arian.kulmer@web.de>
2023-06-25 14:43:55 +0200

    cupid: Import audio configs from V14.0.3.0.TLCMIXM
    Change-Id: I32c54ebd2f55ed06bc49da567d6654f056909f50

--- device.mk
+++ device.mk
@@ -10,6 +10,11 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/cupid/cupid-vendor.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_taro/mixer_paths_overlay_static.xml \
+    $(LOCAL_PATH)/audio/resourcemanager_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_taro/resourcemanager_waipio_mtp.xml
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
