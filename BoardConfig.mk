e315d7b948d0a1aac39577569516416e1decb6db
Arian <arian.kulmer@web.de>
2024-01-27 11:10:12 +0100

    cupid: Add regulator, touch, haptics and audio kernel modules
    Change-Id: I9484a243935db3355a8639a9ae6413dc82431b1c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,20 @@ include vendor/xiaomi/cupid/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/cupid
 
+# Kernel
+device_second_stage_modules := \
+	qcom_pm8008-regulator.ko \
+	fts_touch_spi.ko \
+	aw8697-haptic.ko
+
+device_vendor_dlkm_exclusive_modules := \
+	cs35l41_dlkm.ko
+
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
+BOARD_VENDOR_KERNEL_MODULES_LOAD += $(device_second_stage_modules) $(device_vendor_dlkm_exclusive_modules)
+
+BOOT_KERNEL_MODULES += $(device_second_stage_modules)
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 
