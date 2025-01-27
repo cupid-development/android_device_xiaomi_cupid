4c608f249616e789b00bddd59399ee52001ed0c6
Arian <arian.kulmer@web.de>
2025-01-27 23:21:19 +0100

    cupid: Import audio configs from LA.VENDOR.1.0.r1-26900-WAIPIO.QSSI15.0
    Change-Id: Ie7e6f1c3a41d6b800ae3501c4afff8e898ff8dad

--- device.mk
+++ device.mk
@@ -10,6 +10,12 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/cupid/cupid-vendor.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_taro/mixer_paths_waipio_mtp.xml \
+    $(LOCAL_PATH)/audio/resourcemanager_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_taro/resourcemanager_waipio_mtp.xml \
+    $(LOCAL_PATH)/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResCupid \
