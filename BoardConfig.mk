7f5c686f722deeb2c629d33e9f7a049a7194233b
Arian <arian.kulmer@web.de>
2025-03-30 16:03:46 +0200

    cupid: Enable ultrasound proximity sensor in audio hal
    Change-Id: I7db1afd5ab2686e1f0c70e61ba1afe6fd3ba33ff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ include vendor/xiaomi/cupid/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/cupid
 
+# Audio
+AUDIO_FEATURE_ENABLED_ULTRASOUND_PROXIMITY := true
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
